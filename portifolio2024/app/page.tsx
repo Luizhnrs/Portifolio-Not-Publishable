750ab3dc8fcbba95d85910a9a29f5debf2c3e36f
Luizhnrs <Luizhnrsg@gmail.com>
2023-12-16 14:04:07 -0300

    Feat: Components

--- portifolio2024/app/page.tsx
+++ portifolio2024/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
    <main className='h-fulln w-full'>
-      <div className=''>
+      <div className='flex flex-col gap-20'>
 
       </div>
    </main>
