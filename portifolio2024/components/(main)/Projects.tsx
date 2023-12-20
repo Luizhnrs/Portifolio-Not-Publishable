42c5f29596ed218005b6be3a8d865555430db061
Luizhnrs <Luizhnrsg@gmail.com>
2023-12-20 19:00:53 -0300

    Feat: Projects

---
+++ portifolio2024/components/(main)/Projects.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import ProjectCard from "../sub/ProjectCard";
+
+const Projects = () => {
+  return (
+    <div
+      className="flex flex-col items-center justify-center py-20"
+      id="projects"
+    >
+      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+        My Projects
+      </h1>
+      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
+        <ProjectCard
+          src="/DiscordClone.png"
+          title="Discord Clone"
+          description="
+          A Discord Clone made with Typescript, where the objective is to improve memory consumption on weaker machines"
+        />
+        <ProjectCard
+          src="/HypeCompanion.jpeg"
+          title="HypeCompanion - AI Chat"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+        <ProjectCard
+          src="/SpaceWebsite.png"
+          title="Space Themed Website"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
