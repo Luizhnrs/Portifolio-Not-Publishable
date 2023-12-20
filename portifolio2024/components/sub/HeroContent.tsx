47119c29e14816e6cacc3b743e5fb4e3d8b9b457
Luizhnrs <Luizhnrsg@gmail.com>
2023-12-20 14:19:08 -0300

    Motion Div in Div

--- portifolio2024/components/sub/HeroContent.tsx
+++ portifolio2024/components/sub/HeroContent.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import{motion} from "framer-motion";
 
 const HeroContent = () => {
   return (
-    <div>HeroContent</div>
+    <motion.div
+    initial="hidden"
+    animate="visible"
+    className='flex felx-row items-center justify-center px-20 mt-40 w-full z-[20]'
+    >
+        <div className='h-full w-full flex flex-col gap-5 justify-center m-auto text-start'>
+            <motion.div
+                className='welcome-box py-[15px]'
+            >
+
+            </motion.div>
+        </div>
+    </motion.div>
   )
 }
 
