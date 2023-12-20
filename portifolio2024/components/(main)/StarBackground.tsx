ceb3c8553adf29628f91b08e1302ccc9531fe053
Luizhnrs <Luizhnrsg@gmail.com>
2023-12-20 16:31:48 -0300

    Feat: StarCanvas

--- portifolio2024/components/(main)/StarBackground.tsx
+++ portifolio2024/components/(main)/StarBackground.tsx
@@ -30,3 +30,14 @@ const StarBackground = (props: any) => {
     </group>
   );
 };
+const StarsCanvas = () => (
+    <div className="w-full h-auto fixed inset-0 z-[20]">
+        <Canvas camera={{position: [0, 0, 1]}}>
+        <Suspense fallback={null}>
+            <StarBackground />
+        </Suspense>
+        </Canvas>
+    </div>
+)
+
+export default StarsCanvas;
